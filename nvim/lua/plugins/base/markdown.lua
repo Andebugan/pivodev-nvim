5ee9a680813bbe0d8758b93533886f492fea3aae
andebugan <andreibugaenko2012@mail.ru>
2024-10-10 17:30:04 +0300

    added markdown guide in comments for luasnip latex

--- nvim/lua/plugins/base/markdown.lua
+++ nvim/lua/plugins/base/markdown.lua
@@ -31,6 +31,10 @@ return {
         }
     },
     {
+        -- to generate tree-sitter grammer - install tree-sitter-cli externally
+        -- sudo npm install -g tree-sitter
+        -- sudo npm install -g tree-sitter-cli
+        -- sudo npx tree-sitter-cli generate latex
         "iurimateus/luasnip-latex-snippets.nvim",
         requires = { "L3MON4D3/LuaSnip" },
         config = function()
