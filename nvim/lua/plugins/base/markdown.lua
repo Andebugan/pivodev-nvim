3d63bc6bef841f9bfd9b199517d4418ba31d11a7
andebugan <andreibugaenko2012@mail.ru>
2024-10-14 14:57:41 +0000

    made luasnip for latex in markdown optional

--- nvim/lua/plugins/base/markdown.lua
+++ nvim/lua/plugins/base/markdown.lua
@@ -1,4 +1,4 @@
-return {
+local plugins = {
     {
         -- Install markdown preview, use npx if available.
         "iamcco/markdown-preview.nvim",
@@ -29,17 +29,7 @@ return {
                 -- configuration here or empty for defaults
             },
         }
-    },
-    {
-        -- to generate tree-sitter grammer - install tree-sitter-cli externally
-        -- sudo npm install -g tree-sitter
-        -- sudo npm install -g tree-sitter-cli
-        -- sudo npx tree-sitter-cli generate latex
-        "iurimateus/luasnip-latex-snippets.nvim",
-        requires = { "L3MON4D3/LuaSnip" },
-        config = function()
-            require 'luasnip-latex-snippets'.setup( { use_treesitter = true } )
-            require("luasnip").config.setup { enable_autosnippets = true }
-        end,
     }
 }
+
+return plugins
--- nvim/lua/plugins/tools/latex.lua
+++ nvim/lua/plugins/tools/latex.lua
@@ -2,6 +2,10 @@ local packages = {}
 
 if LANG_INSTALL_CONFIG.markdown_latex then
     table.insert(packages, {
+        -- to generate tree-sitter grammer - install tree-sitter-cli externally
+        -- sudo npm install -g tree-sitter
+        -- sudo npm install -g tree-sitter-cli
+        -- sudo npx tree-sitter-cli generate latex
         "iurimateus/luasnip-latex-snippets.nvim",
         requires = { "L3MON4D3/LuaSnip" },
         config = function()
